aebb458bd65238b341acbef0a823ad9ed465bab4
IIT-Bhupendra <bhupendra.singh.che19@iitbhu.ac.in>
2020-10-10 11:17:07 +0530

    main.php modified & calender, database added

---
+++ docs/partials/_dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "sms_school";
+
+    $conn = mysqli_connect($servername, $username, $password, $database);
+?>
